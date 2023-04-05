6e7dc5f84a1ae445cf84248c259d5bd3d8716c12
Jessie Moura <jessie.moura19@gmail.com>
2023-04-05 14:21:45 -0300

    work: 2023/04/05

--- script.js
+++ script.js
@@ -34,15 +34,41 @@ function readBeforeDot() {
 function showFormat() {
   const format = $("#formatSel").val();
   const number = $("#inputDec").val();
-  const nNumber = format.split(/([a-zA-Z]+)([0-9]+)/).slice(1,3)
+  const nNumber = format.split(/([a-zA-Z]+)([0-9]+)/).slice(1,3)/* testar a perfomace */
+
+  /* let comma = 0;
+  for (let i = 0; i < format.length; i++) {
+    const element = format[i];
+    if (element === ",") {
+      comma++;
+    }
+  } */
+  
+  // const nComma = format.split(',')
+
+  if(format === '0' || format === '#') return $("#pRes").text(`Valor formatado é: ${number}`);
+  
+  // if(comma > 0) return $("#pRes").text(`Valor formatado é: ${nComma[0] === '#'? '0':nComma[0]},${number}`)
 
   if (nNumber[0] === "n" || nNumber[0] === "N") {
     const iDec = nNumber[1];
     const formNumber = arFormat(Number(number), iDec);
     return $("#pRes").text(`Valor formatado com N é: ${formNumber}`);
   }
-
+  const s = formatNumDelphi
   const iDec = readAfterDot(format);
   const formNumber = arFormat(Number(number), iDec);
   return $("#pRes").text(`Valor formatado é: ${formNumber}`);
+}
+/**
+    * formatNumDelphi
+    * Return a string to Delphi Format
+    *  @type {(sFormat: string, nNumber: number) => string} TypeScript syntax
+    *  @type {formatNumDelphi(string, number): string} Closure syntax
+    * 
+    */
+const formatNumDelphi = (sFormat, nNumber) => {
+  const iDec = readAfterDot(sFormat);
+  const sNumber = arFormat(nNumber, iDec);
+  return sNumber;
 }
